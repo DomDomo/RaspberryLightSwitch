f2493ba4661f164439a89ce35c1708cac53a5bb9
DomDomo <g.domis02@gmail.com>
2021-02-03 18:36:48 +0200

    Initial commit

---
+++ relayTest.py
@@ -0,0 +1,18 @@
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+print("1")
+GPIO.output(18, GPIO.LOW)
+print("2")
+time.sleep(5)
+print("3")
+GPIO.output(18, GPIO.HIGH)
+print("4")
+time.sleep(5)
+print("5")
+GPIO.cleanup()
